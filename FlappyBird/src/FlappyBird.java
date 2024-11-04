e2347097bace922a0be4af527b41d010a1e5cbd8
Nethsara Prabash <138272058+nethsaraPrabash@users.noreply.github.com>
2024-11-04 23:04:54 +0530

    maint initial commit

---
+++ FlappyBird/src/FlappyBird.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class FlappyBird extends JPanel{
+    int boardWidth = 360;
+    int boardHeight = 640;
+
+    Image backgroundImg;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
+}
--- FlappyBird/src/Main.java
+++ FlappyBird/src/Main.java
@@ -1,7 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import javax.swing.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.io.IOException;
+
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Flappy Bird");
+    public static void main(String[] args) throws Exception{
+        int boardWidth = 360;
+        int boardHeight = 640;
+
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setVisible(true);
+
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
     }
 }
